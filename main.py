f0b63e6bf8913d7584e69df295f4e5ed2ea244a2
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2024-08-08 02:03:29 +0530

    hi

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ async def fcast(_, m : Message):
 print("BOT SUCCESSFULLY DEPLOYED !!")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
 
 app.run()
